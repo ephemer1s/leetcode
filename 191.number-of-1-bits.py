314bb18972c781393f07c86bb121fe67106e367d
ephemer1s <haochengvanadis@gmail.com>
2022-05-26 18:40:25 -0400

    Update 191.number-of-1-bits.py
    oneliner

--- 191.number-of-1-bits.py
+++ 191.number-of-1-bits.py
@@ -7,11 +7,6 @@
 # @lc code=start
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        ans = 0
-        while n > 0:
-            if n % 2 == 1:
-                ans += 1
-            n = n // 2
-        return ans
+        return '{:b}'.format(n).count('1')
 # @lc code=end
 
