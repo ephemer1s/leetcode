a6a94b750080157dda4ee97d2d6381e88102208c
Spica TSUKIMI <haochengvanadis@gmail.com>
2022-07-26 20:37:01 -0400

    Reversed Preorder Traversal

---
+++ 114.flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=114 lang=python3
+#
+# [114] Flatten Binary Tree to Linked List
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.prev = None
+        
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if root == None:
+            return
+        self.flatten(root.right)
+        self.flatten(root.left)
+        root.right = self.prev
+        root.left = None
+        self.prev = root
+        
+# @lc code=end
+
