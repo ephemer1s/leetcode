0625149d2dfccd9d6f1497a476d7ec01e8392771
Spica TSUKIMI <haochengvanadis@gmail.com>
2022-08-09 04:22:59 -0400

    Create 16.3-sum-closest.py

---
+++ 16.3-sum-closest.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=16 lang=python3
+#
+# [16] 3Sum Closest
+#
+
+# @lc code=start
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        ret = nums[0] + nums[1] + nums[2]
+        for i in range(len(nums) - 2):
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum == target:
+                    return sum
+                elif sum > target:
+                    if abs(sum - target) < abs(ret - target):
+                        ret = sum
+                    k = k - 1
+                elif sum < target:
+                    if abs(sum - target) < abs(ret - target):
+                        ret = sum
+                    j = j + 1
+        return ret
+            
+# @lc code=end
+
